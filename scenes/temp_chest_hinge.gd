03bca7cf00900764955dc4895c5c8f0fcd45c8bc
Sam G. <104700099+hey-1m-sam@users.noreply.github.com>
2026-02-23 13:56:25 -0600

    Refactor chest hinge rotation logic with tweening
    yeah what the autogenerated title said
    
    Signed-off-by: Sam G. <104700099+hey-1m-sam@users.noreply.github.com>

--- scenes/temp_chest_hinge.gd
+++ scenes/temp_chest_hinge.gd
@@ -2,7 +2,6 @@ extends Node3D
 
 
 func _on_chest_body_entered(body: Node3D) -> void:
-	for i in range(135):
-		self.rotation.x -= deg_to_rad(1)
-		print()
-		await get_tree().create_timer(0.001).timeout 
+	if body.is_in_group("player"):
+		var tween := create_tween()
+		tween.tween_property(self, "rotation:x", self.rotation.x - deg_to_rad(135), 0.135)
