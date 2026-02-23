4bb33d00e640d2b113ce25b1e071b220664e7fef
Sam G. <104700099+hey-1m-sam@users.noreply.github.com>
2026-02-23 13:57:29 -0600

    Add collection check for power-ups
    Prevent multiple collections of the same power-up.
    sure what was autogenerated
    
    Signed-off-by: Sam G. <104700099+hey-1m-sam@users.noreply.github.com>

--- scenes/power_up.gd
+++ scenes/power_up.gd
@@ -1,10 +1,16 @@
 extends Area3D
 
 var power
+var collected: bool = false
 
 func _ready() -> void:
 	power = Global.POWER_UPS.pick_random()
 
+
 func _on_body_entered(body: Node3D) -> void:
-	if self.visible:
-		Global.inventory.append(power)
+	if collected:
+		return
+	collected = true
+	monitoring = false
+	visible = false
+	Global.inventory.append(power)
