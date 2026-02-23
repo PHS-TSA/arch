3e20b8e056a4c9a3894b995eb26c96f5bf5046e2
Sam G. <104700099+hey-1m-sam@users.noreply.github.com>
2026-02-23 13:49:00 -0600

    Delete scenes/constants.gd
    Signed-off-by: Sam G. <104700099+hey-1m-sam@users.noreply.github.com>

--- scenes/constants.gd
+++
@@ -1,6 +0,0 @@
-extends Node3D
-
-# This script is reserved for shared scene/game constants.
-# It currently has no logic, but is kept as a dedicated place
-# to define constants that can be accessed from the editor
-# or other scripts in future development.
