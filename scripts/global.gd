5d91e640cc1eb7a12b1f6655ada63dc94a48d57c
Sam G. <104700099+hey-1m-sam@users.noreply.github.com>
2026-02-23 13:58:25 -0600

    Remove Debug Items
    Signed-off-by: Sam G. <104700099+hey-1m-sam@users.noreply.github.com>

--- scripts/global.gd
+++ scripts/global.gd
@@ -2,8 +2,3 @@ extends Node3D
 
 const POWER_UPS = ["freeze", "speed", "slow", "jump", "shoot"]
 var inventory = [] # Tanish you can decide the inventory stuff but I made it a list that it's just putting stuff into now
-
-func _process(delta):
-	print("FPS: %s" % Engine.get_frames_per_second())
-	print("TPS: %s" % Engine.physics_ticks_per_second)
-	print("FDT: %s" % Engine.get_frames_drawn())
