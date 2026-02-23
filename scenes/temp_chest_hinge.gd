c96c4ddae601ee14f6ef65e8692dc54e0f9c6658
Eli <88557639+lishaduck@users.noreply.github.com>
2026-02-23 17:26:59 -0600

    fix: use is player not in group

--- scenes/temp_chest_hinge.gd
+++ scenes/temp_chest_hinge.gd
@@ -2,6 +2,6 @@ extends Node3D
 
 
 func _on_chest_body_entered(body: Node3D) -> void:
-	if body.is_in_group("player"):
+	if body is Player:
 		var tween := create_tween()
 		tween.tween_property(self, "rotation:x", self.rotation.x - deg_to_rad(135), 0.135)
