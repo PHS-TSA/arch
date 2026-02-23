cf30c6091b91b89b006768078f4d4d884fae93cd
Sam G. <104700099+hey-1m-sam@users.noreply.github.com>
2026-02-23 13:52:29 -0600

    Fix (see comments)
    a
    
    Signed-off-by: Sam G. <104700099+hey-1m-sam@users.noreply.github.com>

--- scenes/power_up.gd
+++ scenes/power_up.gd
@@ -1,8 +1,10 @@
 extends Area3D
 
-var power = Global.POWER_UPS.pick_random()
+var power
+
+func _ready() -> void:
+	power = Global.POWER_UPS.pick_random()
 
 func _on_body_entered(body: Node3D) -> void:
 	if self.visible:
 		Global.inventory.append(power)
-		print(Global.inventory)
